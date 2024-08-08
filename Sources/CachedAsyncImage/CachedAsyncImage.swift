7ad93f8e8246908943af0008ebe897816053a7aa
Alex Nagy <rebeloperalpar@gmail.com>
2024-08-08 11:05:22 +0300

    first commit

--- Sources/CachedAsyncImage/CachedAsyncImage.swift
+++ Sources/CachedAsyncImage/CachedAsyncImage.swift
@@ -1,2 +1,87 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+fileprivate class ImageCache {
+    static private var cache: [URL: Image] = [:]
+    static subscript(url: URL) -> Image? {
+        get {
+            ImageCache.cache[url]
+        }
+        set {
+            ImageCache.cache[url] = newValue
+        }
+    }
+}
+
+public struct CachedAsyncImage<Content>: View where Content: View{
+
+    private let url: URL?
+    private let scale: CGFloat
+    private let transaction: Transaction
+    private let content: (AsyncImagePhase) -> Content
+    
+    /// Loads, displays and caches a modifiable image from the specified URL in phases.
+    ///
+    /// If you set the asynchronous image's URL to `nil`, or after you set the
+    /// URL to a value but before the load operation completes, the phase is
+    /// ``AsyncImagePhase/empty``. After the operation completes, the phase
+    /// becomes either ``AsyncImagePhase/failure(_:)`` or
+    /// ``AsyncImagePhase/success(_:)``. In the first case, the phase's
+    /// ``AsyncImagePhase/error`` value indicates the reason for failure.
+    /// In the second case, the phase's ``AsyncImagePhase/image`` property
+    /// contains the loaded image. Use the phase to drive the output of the
+    /// `content` closure, which defines the view's appearance:
+    ///
+    ///     CachedAsyncImage(url: URL(string: "https://example.com/icon.png")) { phase in
+    ///         if let image = phase.image {
+    ///             image // Displays the loaded image.
+    ///         } else if phase.error != nil {
+    ///             Color.red // Indicates an error.
+    ///         } else {
+    ///             Color.blue // Acts as a placeholder.
+    ///         }
+    ///     }
+    ///
+    /// To add transitions when you change the URL, apply an identifier to the
+    /// ``AsyncImage``.
+    ///
+    /// - Parameters:
+    ///   - url: The URL of the image to display.
+    ///   - scale: The scale to use for the image. The default is `1`. Set a
+    ///     different value when loading images designed for higher resolution
+    ///     displays. For example, set a value of `2` for an image that you
+    ///     would name with the `@2x` suffix if stored in a file on disk.
+    ///   - transaction: The transaction to use when the phase changes.
+    ///   - content: A closure that takes the load phase as an input, and
+    ///     returns the view to display for the specified phase.
+    public init(url: URL?,
+                scale: CGFloat = 1.0,
+                transaction: Transaction = Transaction(),
+                @ViewBuilder content: @escaping (AsyncImagePhase) -> Content) {
+        self.url = url
+        self.scale = scale
+        self.transaction = transaction
+        self.content = content
+    }
+
+    public var body: some View {
+        if let url, let cached = ImageCache[url] {
+            let _ = print("cached")
+            content(.success(cached))
+        } else {
+            let _ = print("fetched")
+            AsyncImage(url: url,
+                scale: scale,
+                transaction: transaction) { phase in
+                cacheAndRender(phase: phase)
+            }
+        }
+    }
+    
+    private func cacheAndRender(phase: AsyncImagePhase) -> some View {
+        if case .success (let image) = phase, let url {
+            ImageCache[url] = image
+        }
+        return content(phase)
+    }
+}
--- Tests/CachedAsyncImageTests/CachedAsyncImageTests.swift
+++ Tests/CachedAsyncImageTests/CachedAsyncImageTests.swift
@@ -1,12 +1,2 @@
 import XCTest
 @testable import CachedAsyncImage
-
-final class CachedAsyncImageTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
-}
